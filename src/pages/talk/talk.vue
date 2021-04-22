6b3e8a01e3ae9c2ed9435572e1c6b2eff3818e69
smallsmartlc <1346683197@qq.com>
2021-04-22 22:28:54 +0800

    code format

--- src/pages/talk/talk.vue
+++ src/pages/talk/talk.vue
@@ -5,8 +5,10 @@
 				<editor id="editor" class="ql-container" placeholder="说点什么吧..."></editor>
 			</view>
 			<view class="button-box">
-				<u-upload :action="action" :file-list="fileList" custom-btn >
-					<view class="button" slot="addBtn"><u-icon name="photo" size="48"></u-icon></view>
+				<u-upload :action="action" :file-list="fileList" custom-btn>
+					<view class="button" slot="addBtn">
+						<u-icon name="photo" size="48"></u-icon>
+					</view>
 				</u-upload>
 			</view>
 		</view>
@@ -19,33 +21,34 @@
 		data() {
 			return {
 				tabbar: null,
-				fileList : [],
+				fileList: [],
 			}
 		},
-		onLoad() {
-		},
+		onLoad() {},
 		onShow() {
 			// 加载导航栏参数
 			this.tabbar = getApp().globalData.tabbar;
 		},
 		methods: {
-			
+
 		}
 	}
 </script>
 
 <style scoped>
-.container{
-	padding: 16rpx;
-}
-#editor{
-	border-bottom: 1px solid #e7e7e7;
-	height: 250rpx;
-}
-.button-box{
-	display: flex;
-	height: 80rpx;
-	border-bottom: 1px solid #e7e7e7;
-	align-items: center;
-}
+	.container {
+		padding: 16rpx;
+	}
+
+	#editor {
+		border-bottom: 1px solid #e7e7e7;
+		height: 250rpx;
+	}
+
+	.button-box {
+		display: flex;
+		height: 80rpx;
+		border-bottom: 1px solid #e7e7e7;
+		align-items: center;
+	}
 </style>
