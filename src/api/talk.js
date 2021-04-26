555b79ad22d3403ba7a13b968785597244a30ab6
smallsmartlc <1346683197@qq.com>
2021-04-26 23:08:33 +0800

    增加talk

---
+++ src/api/talk.js
@@ -0,0 +1,28 @@
+import request from '@/utils/request'
+
+export const pageTalk = (cur,size) => request({
+    method: 'get',
+    url: `/talk/page/${cur}/${size}`,
+})
+export const pageComment = (objId,cur,pageSize) => request({
+    method : 'get',
+    url: `/comment/1/${objId}/${cur}/${pageSize}`,
+})
+export const deleteTalk = (id) => request({
+    method : 'delete',
+    url: `/talk/${id}`
+})
+export const getTalkById = (id) => request({
+    method: 'get',
+    url: `/talk/${id}`,
+})
+export const pageTalkList = (params) => request({
+    method: 'get',
+    url: `/talk/page/user`,
+    params : params,
+})
+export const addTalk = (text) => request({
+    method: 'post',
+    url: `/talk`,
+    params : {text},
+})
--- src/pages/talk/talk.vue
+++ src/pages/talk/talk.vue
@@ -5,12 +5,17 @@
 				<editor id="editor" class="ql-container" placeholder="说点什么吧..."></editor>
 			</view>
 			<view class="button-box">
-				<u-upload ref="uUpload" :before-upload="beforeUpload" 
-				:before-remove = "beforeRemove" custom-btn>
-					<view class="button" slot="addBtn">
-						<u-icon name="photo" size="48"></u-icon>
-					</view>
-				</u-upload>
+				<view>
+					<u-upload ref="uUpload" :before-upload="beforeUpload" :before-remove="beforeRemove" custom-btn>
+						<view class="button" slot="addBtn">
+							<u-icon name="photo" size="48"></u-icon>
+						</view>
+					</u-upload>
+				</view>
+				<view>
+					<u-button :loading="submitting" @click="submit" type="primary" shape="circle" size="mini">发送
+					</u-button>
+				</view>
 			</view>
 		</view>
 		<u-toast ref="uToast" />
@@ -19,13 +24,20 @@
 </template>
 
 <script>
-	import { BaseUrl } from '@/constants/index'
+	import {
+		BaseUrl
+	} from '@/constants/index'
+	import {
+		pageTalk,
+		addTalk
+	} from '@/api/talk'
 	export default {
 		data() {
 			return {
 				tabbar: null,
-				imgList : [],
-				action : BaseUrl + '/file/img/upload/talkimg',
+				imgList: [],
+				action: BaseUrl + '/file/img/upload/talkimg',
+				submitting: false,
 			}
 		},
 		onLoad() {},
@@ -34,36 +46,67 @@
 			this.tabbar = getApp().globalData.tabbar;
 		},
 		methods: {
-			beforeRemove(index,list){
-				this.imgList.splice(index,1);
+			beforeRemove(index, list) {
+				this.imgList.splice(index, 1);
 			},
-			beforeUpload(index, list){
+			beforeUpload(index, list) {
 				// 在这里自定义上传逻辑
 				uni.uploadFile({
-					url : BaseUrl + '/file/img/upload/talkimg',
-					filePath:list[index].url,
+					url: BaseUrl + '/file/img/upload/talkimg',
+					filePath: list[index].url,
 					name: 'file',
 					success: (res) => {
 						let temp = JSON.parse(res.data)
 						console.log(temp);
-						if(temp.code == '200'){
+						if (temp.code == '200') {
 							this.imgList.push(temp.data);
-						}else{
+						} else {
 							this.$refs.uToast.show({
 								type: "error",
-								message: '图片上传失败',
+								title: '图片上传失败',
 							})
 						}
 					},
 					fail: (err) => {
 						this.$refs.uToast.show({
 							type: "error",
-							message: '图片上传失败',
+							title: '图片上传失败',
 						})
 					}
-					
+
 				})
 				return false;
+			},
+			submit() {
+				this.submiting = true;
+				let text = "";
+				// todo 获取编辑器中的html内容
+				addTalk(text).then((res) => {
+					if (res.code == 200) {
+						this.$refs.uToast.show({
+							title: '发帖成功!',
+							type: 'success'
+						});
+						var temp = {
+							author: getApp().globalData.userInfo,
+							hit: {
+								collect: false,
+								comments: 0,
+								thumb: false,
+								thumbs: 0,
+							},
+							id: res.data,
+							text: text,
+						}
+						this.talks.unshift(temp);
+					} else {
+						this.$message({
+							title: '发送失败!',
+							type: 'error'
+						});
+					}
+					this.submiting = false;
+				})
 			}
 		}
 	}
@@ -84,5 +127,6 @@
 		border-bottom: 1px solid #e7e7e7;
 		align-items: center;
 		padding: 10rpx 20rpx;
+		justify-content: space-between;
 	}
 </style>
